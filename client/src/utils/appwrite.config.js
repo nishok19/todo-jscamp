4c2ef213d6a4ff86875b9f9e5e98dc5e16170853
nishok19 <nishokrt1905@gmail.com>
2022-12-30 19:26:54 +0530

    Minor Changes

--- client/src/utils/appwrite.config.js
+++ client/src/utils/appwrite.config.js
@@ -2,6 +2,6 @@ import { Client, Account } from "appwrite";
 
 const client = new Client();
 
-client.setEndpoint("http://localhost/v1").setProject("6395e30bb7bddc91a23b");
+client.setEndpoint("http://localhost/v1").setProject("63aee57acdcf19db7e39");
 
 export const account = new Account(client);
