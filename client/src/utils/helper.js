b6495016fd5f3c861604160768f26cf3f63c45ef
nishok19 <nishokrt1905@gmail.com>
2023-01-14 03:01:03 +0530

    bug fix.

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -40,9 +40,9 @@ export const searchTodo = (todos, text) => {
 
 export const getBaseUrl = () => {
   const prodApiUrl = "https://servertaskmaster.up.railway.app/";
+  return prodApiUrl;
   if (process.env.NODE_ENV === "development")
     return process.env.REACT_APP_BACKEND_TEST_API_URL;
   else if (process.env.NODE_ENV === "production")
     return process.env.REACT_APP_BACKEND_PROD_API_URL;
-  else return prodApiUrl;
 };
